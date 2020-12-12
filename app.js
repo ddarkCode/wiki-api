a9288f041b0f3085407b1c6148504366c4c760ce
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-12-12 16:25:08 +0100

    Complete RESTful API course code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set( 'view engine', 'ejs' );
 app.use( express.static( 'public' ) );
 app.use( express.urlencoded( { extended: true } ) );
 
-mongoose.connect( 'mongodb://localhost27017/wikiDB' );
+mongoose.connect( 'mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true} );
 
 const articlesSchema = new mongoose.Schema( {
 	title: String,
@@ -19,12 +19,81 @@ const articlesSchema = new mongoose.Schema( {
 
 const Article = mongoose.model( 'article', articlesSchema );
 
-app.get( '/', ( req, res ) => {
-	Article.find( {}, ( err, response ) => {
-		if ( !err ) {
-			res.send( response );
+
+app.route('/articles')
+.get((req, res) => {
+	Article.findOne({title: req.params.title}, (err, foundArticles) => {
+		if ( !err ){
+			res.send( foundArticles );
+		} else {
+			console.log( err );
 		}
 	})
-})
+} )
+.post(  ( req, res ) => {
+	console.log( req.body );
+	const { title, content } = req.body;
+	const newArticle = new Article( {
+		title: title,
+		content: content
+	} );
+	
+	newArticle.save( ( err, result ) => {
+		if ( !err ) {
+			res.send( result );
+		}
+	} );
+} )
+
+.delete( ( req, res ) => {
+		Article.deleteMany( {}, ( err ) => {
+			if ( !err ) {
+				res.send( 'Successfully deleted all articles' );
+			}
+		} );
+	} );
+
+
+
+
+app.route( '/articles/:title' )
+	.get( ( req, res ) => {
+		const { title } = req.params;
+		Article.find( { title: title }, ( err, foundArticle ) => {
+			if ( !err ) {
+				console.log( foundArticle );
+				res.send( foundArticle );
+			} else {
+				res.send( err );
+			}
+		} );
+	} )
+	.put( ( req, res ) => {
+		const { title } = req.params;
+		Article.update( { title: title }, { title: req.body.title, content: req.body.content }, { overwrite: true }, ( err, raw ) => {
+			if ( !err ) {
+				console.log( 'Successfully updated the article' );
+				res.send( raw );
+			}
+		} );
+	} )
+	.patch( ( req, res ) => {
+		const { title } = req.params;
+		Article.update( { title: title }, req.body, ( err, raw ) => {
+			if ( !err ) {
+				console.log( raw );
+				res.send( 'Successfully updated the article' );
+			}
+		} );
+	} )
+	.delete( ( req, res ) => {
+		Article.deleteOne( { title: req.params.title }, ( err ) => {
+			if ( !err ) {
+				res.send( 'Successfully deleted the selected article.' );
+			}
+		} );
+	} );
+
+
 
 app.listen( 3000, () => console.log( 'Server is running on port 3000' ) );
